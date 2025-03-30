a90745c73aad1d69b751c2eb3f062d524de2fe64
Brandon <brandon.s.mckinley@gmail.com>
2025-03-30 08:25:58 -0700

    working on loop
    Update uniqueArray.cpp

--- uniqueArray.cpp
+++ uniqueArray.cpp
@@ -1,30 +1,28 @@
 #include <iostream>
 using namespace std;
 
-
-//Function prototypes
-
-int main()
+int main ()
 {
     const int SIZE = 20; 
     int unique[SIZE] = {0};
 
-    cout << "Please choose 20 integers to input: " << endl;
+    cout << "Please enter 20 unique integers: " << endl;
 
     for (int i = 0; i < SIZE; i++)
     {
         cin >> unique[i];
         if (unique[i] >= 10 && unique[i] <= 100)
-        {
-            unique[i] = unique[i];
-        } else
-            cout << "Invalid Number!" << endl; 
+            for (int j = 0; j < SIZE; j++)
+            {
+                if (unique[j] != unique[i])
+                     unique[j] = unique[i];
+                 else
+                     cout << "Duplicate Number!" << endl; 
+            }
+               
+         else
+            cout << "Invalid Number!"<< endl;            
     }
-
-    //for (int i = 0; i < SIZE; i++){cout << unique[i] << " ";}
-
-
+           
     return 0;
 }
-
-//Functions
