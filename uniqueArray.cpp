8c98c39457ba9593504d2217c3324b971abf02d5
Brandon <brandon.s.mckinley@gmail.com>
2025-03-30 13:33:08 -0700

    Update uniqueArray.cpp
    fixed loop for determining what numbers are unique, also had issue with first input not being read and outputting as a zero

--- uniqueArray.cpp
+++ uniqueArray.cpp
@@ -5,24 +5,33 @@ int main ()
 {
     const int SIZE = 20; 
     int unique[SIZE] = {0};
+    int number;
 
     cout << "Please enter 20 unique integers: " << endl;
 
     for (int i = 0; i < SIZE; i++)
     {
-        cin >> unique[i];
-        if (unique[i] >= 10 && unique[i] <= 100)
-            for (int j = 0; j < SIZE; j++)
-            {
-                if (unique[j] != unique[i])
-                     unique[j] = unique[i];
-                 else
-                     cout << "Duplicate Number!" << endl; 
-            }
+        cin >> number;
+        if (number >= 10 && number <= 100)
+        {
+                unique[i] = number;
+                for (int j = 0; j < i ; j++)
+                {
+                    if (number == unique[j])
+                    {
+                        cout << "Duplicate Number!" << endl; 
+                        i--; // allows to program to continue until the user inputs a proper number
+                        break; // exits if duplicate is found
+                    }  else
+                        unique[i] = number;
+                }
                
-         else
-            cout << "Invalid Number!"<< endl;            
+        } else 
+            cout << "Invalid Number" << endl;              
     }
+
+    cout << "The following is the array with only unique integers: ";
+    for (int i = 0; i < SIZE; i++){cout << unique[i] << " ";}
            
     return 0;
 }
