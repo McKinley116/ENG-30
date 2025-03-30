9a69f1e6964be87e3224d6b931a1452a1f64ba11
Brandon <brandon.s.mckinley@gmail.com>
2025-03-30 13:56:02 -0700

    Finished uniqueArray.cpp
    completed project. added int duplicate = 0 to track what is a duplicate. i-- to allow user to continue to enter a number

--- uniqueArray.cpp
+++ uniqueArray.cpp
@@ -1,37 +1,44 @@
 #include <iostream>
 using namespace std;
 
-int main ()
-{
+int main() {
     const int SIZE = 20; 
     int unique[SIZE] = {0};
     int number;
 
     cout << "Please enter 20 unique integers: " << endl;
 
-    for (int i = 0; i < SIZE; i++)
+    for (int i = 0; i < SIZE; i++) 
     {
         cin >> number;
-        if (number >= 10 && number <= 100)
-        {
-                unique[i] = number;
-                for (int j = 0; j < i ; j++)
-                {
-                    if (number == unique[j])
-                    {
-                        cout << "Duplicate Number!" << endl; 
-                        i--; // allows to program to continue until the user inputs a proper number
-                        break; // exits if duplicate is found
-                    }  else
-                        unique[i] = number;
+        if (number >= 10 && number <= 100) {
+            int duplicate = 0; // 0 means no duplicate found
+
+            for (int j = 0; j < i; j++) 
+            {
+                if (number == unique[j]) // Check for duplicate
+                { 
+                    cout << "Duplicate Number!" << endl;
+                    i--; // Allow user to enter a valid number again
+                    duplicate = 1;
+                    break; // Exit loop if duplicate is found
                 }
-               
-        } else 
-            cout << "Invalid Number" << endl;              
+            }
+
+            if (duplicate == 0) { // Store only if it's not a duplicate
+                unique[i] = number;
+            }
+        } else {
+            cout << "Invalid Number" << endl;
+            i--; // Allow user to enter a valid number again
+        }
     }
 
     cout << "The following is the array with only unique integers: ";
-    for (int i = 0; i < SIZE; i++){cout << unique[i] << " ";}
-           
+    for (int i = 0; i < SIZE; i++) 
+    {
+        cout << unique[i] << " ";
+    }
+    
     return 0;
 }
